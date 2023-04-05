3d749e6d26c4721148241c2a323d5c6d48ca27fe
Jankes <jankes@jankes.com.pl>
2023-04-05 15:04:24 +0200

    2.33

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,36 +1,15 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Controllers;
 
-use App\App;
 use App\View;
-use App\Models\Invoice;
-use App\Models\SignUp;
-use App\Models\User;
 
 class HomeController
 {
     public function index(): View
     {
-        $email = 'user@mail.com';
-        $name = "User Name";
-        $amount = 12.5;
-
-        $userModel = new User();
-        $invoiceModel = new Invoice();
-
-        $invoiceId = (new SignUp($userModel, $invoiceModel))->register(
-            [
-                'email' => $email,
-                'name' => $name
-            ],
-            [
-                'amount' => $amount,
-            ]
-        );
-
-        return View::make('index', ['invoice' => $invoiceModel->find($invoiceId)]);
+        return View::make('index');
     }
 }
