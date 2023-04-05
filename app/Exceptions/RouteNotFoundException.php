2f318590976480d0efbd8df9731ea5dadc492449
Jankes <jankes@jankes.com.pl>
2023-04-05 19:23:35 +0200

    Corrected Exceptions

--- app/Exceptions/RouteNotFoundException.php
+++ app/Exceptions/RouteNotFoundException.php
@@ -1,12 +1,10 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Exceptions;
 
 class RouteNotFoundException extends \Exception
 {
     protected $message = '404 ni ma';
-    // public function __construct()
-    // {
-    //     echo $this->message;
-    // }
 }
