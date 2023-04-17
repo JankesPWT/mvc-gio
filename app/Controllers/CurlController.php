7e72fb7d8eeec7d1e43294ce1f21e5a24f60c1ac
Jankes <jankes@jankes.com.pl>
2023-04-17 21:04:04 +0200

    3.21 Curl - correct controller

--- app/Controllers/CurlController.php
+++ app/Controllers/CurlController.php
@@ -5,22 +5,35 @@ declare(strict_types = 1);
 namespace App\Controllers;
 
 use App\Attributes\Get;
-use App\Contracts\EmailValidationInterface;
 
 class CurlController
 {
-    public function __construct(private EmailValidationInterface $emailValidationService)
-    {
-    }
-
     #[Get('/curl')]
     public function index()
     {
-        $email  = 'programwithgio@gmail.com';
-        $result = $this->emailValidationService->verify($email);
+        $handle = curl_init();
 
-        echo '<pre>';
-        print_r($result);
-        echo '</pre>';
+        $apiKey = $_ENV['EMAILABLE_API_KEY'];
+        $email  = 'programwithgio@gmail.com';
+
+        $params = [
+            'api_key' => $apiKey,
+            'email'   => $email,
+        ];
+
+        $url = 'https://api.emailable.com/v1/verify?' . http_build_query($params);
+
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+        $content = curl_exec($handle);
+
+        if ($content !== false) {
+            $data = json_decode($content, true);
+
+            echo '<pre>';
+            print_r($data);
+            echo '</pre>';
+        }
     }
 }
